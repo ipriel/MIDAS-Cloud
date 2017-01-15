4cf600aeadc4c30e0ce81577446eab4168d8db9c
Itamar Priel <itamar.priel@gmail.com>
2017-01-15 13:50:59 +0200

    Fixed routing bug on refresh - Removed Express.static setting - Modified base routes to seperate:    - navigation related routes (html pages)    - asset related routes (js, css, etc.)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,19 @@
+var path = require('path');
 var userRoutes = require('./user.route');
 var infoRoutes = require('./info.route');
 
-module.exports = function(app) {
+module.exports = function(app, rootDir) {
     app.use('/api/user', userRoutes);
     app.use('/api/info', infoRoutes);
 
+    // Catch all requests for assets (js, css, etc.)
+    app.get('*.*$', function(req, res) {
+        console.log(req.path);
+        res.sendFile(path.join(rootDir, 'www', req.path));
+    });
+
     // Catch all other routes and return the index file
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '/www/index.html'));
+        res.sendFile(path.join(rootDir, 'www', 'index.html'));
     });
 };
